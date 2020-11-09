fd5fe9c00c41ae19d4d8b4d6eb153ea8f864c67d
Igor Kucherenko <igor.kucherenko.dev@gmail.com>
2020-11-10 00:04:22 +0300

    Rework game board and add move tests

--- src/core/game.test.ts
+++ src/core/game.test.ts
@@ -1,15 +1,73 @@
-import {Game} from './game';
+import {Point, Game, Player} from './game';
 
 describe('Game', () => {
-    it('should generate board', () => {
+    let game: Game;
+
+    beforeEach(() => {
+        game = new Game();
+    });
+
+    it('should return empty game state', () => {
+        const board = game.getState();
+
         const expected = [
-            [''], [''], [''],
-            [''], [''], [''],
-            [''], [''], [''],
+            ['', '', ''],
+            ['', '', ''],
+            ['', '', ''],
         ];
 
-        const board = new Game().generateBoard();
-
         expect(board).toEqual(expected);
+    });
+
+    it('should set apply move correctly', () => {
+        interface Move {
+            point: Point;
+            player: Player;
+            expected: string[][];
+        }
+
+        const moves: Move[] = [
+            {
+                point: {x: 0, y: 0},
+                player: 'x',
+                expected: [
+                    ['x', '', ''],
+                    ['', '', ''],
+                    ['', '', ''],
+                ],
+            },
+            {
+                point: {x: 2, y: 2},
+                player: 'o',
+                expected: [
+                    ['x', '', ''],
+                    ['', '', ''],
+                    ['', '', 'o'],
+                ],
+            },
+            {
+                point: {x: 2, y: 1},
+                player: 'x',
+                expected: [
+                    ['x', '', ''],
+                    ['', '', 'x'],
+                    ['', '', 'o'],
+                ],
+            },
+            {
+                point: {x: 1, y: 2},
+                player: 'o',
+                expected: [
+                    ['x', '', ''],
+                    ['', '', 'x'],
+                    ['', 'o', 'o'],
+                ],
+            }
+        ];
+
+        moves.forEach(({point, player, expected}: Move) => {
+            game.move(point, player);
+            expect(game.getState()).toEqual(expected);
+        });
     });
 });
--- src/core/game.ts
+++ src/core/game.ts
@@ -1,9 +1,24 @@
+export interface Point {
+    x: number;
+    y: number;
+}
+
+export type Player = 'x' | 'o';
+
+const INITIAL_BOARD = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', ''],
+];
+
 export class Game {
-    public generateBoard(): string[][] {
-        return [
-            [''], [''], [''],
-            [''], [''], [''],
-            [''], [''], [''],
-        ];
+    private _board = INITIAL_BOARD;
+
+    public getState(): string[][] {
+        return this._board;
+    }
+
+    public move({x, y}: Point, player: Player): void {
+        this._board[y][x] = player;
     }
 }
